e5efe083c881462f6f8d31c7dcb7087df29ce155
Júlio Marques <juliomarques170@gmail.com>
2023-08-29 23:04:44 -0300

    Primeiro commit

---
+++ src/test/java/com/seguros/next/desafio/spring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguros.next.desafio.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
